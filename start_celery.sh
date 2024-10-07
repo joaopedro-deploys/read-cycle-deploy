ea243b53a63db5f378d72218fae38e1813ec850d
João Pedro <joaogood@outlook.com>
2024-10-07 10:05:57 -0300

    atulizando start_celery

--- start_celery.sh
+++ start_celery.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Aguardar alguns segundos para garantir que o App Service esteja inicializado
 
-celery -A project worker --pool=solo -l info &
+celery -A project worker --pool=solo -l info 
 
 sleep 30
