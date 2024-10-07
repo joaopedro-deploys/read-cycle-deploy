36676b9768d8ae8da01e89e6feffc7639215bf27
João Pedro <joaogood@outlook.com>
2024-10-07 09:30:57 -0300

    atulizando startcommand

--- start_celery.sh
+++ start_celery.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Aguardar alguns segundos para garantir que o App Service esteja inicializado
 
+python manage.py runserver --no-output --
+
 sleep 30
 nohup celery -A your_app_name worker --loglevel=info > celery.log 2>&1 &
