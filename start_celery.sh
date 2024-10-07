c9503b2b6fb4227424476d6533e8547127b62826
João Pedro <joaogood@outlook.com>
2024-10-07 09:52:11 -0300

    adicionando gunicorn no init

--- start_celery.sh
+++ start_celery.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Aguardar alguns segundos para garantir que o App Service esteja inicializado
 
-python manage.py runserver 0.0.0.0:8000
+python manage.py makemigrations
+python manage.py migrate
+
+exec gunicorn project.wsgi:application 
 
 sleep 30
 celery -A project worker --pool=solo -l info
